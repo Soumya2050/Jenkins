5569720b5d1839bb3d171752b08d1c608f9f65a9
Soumya2050 <srjena2050@gmail.com>
2023-07-13 10:25:43 +0530

    1st cojzJmxcbndvrheg

---
+++ JenkinsEXE/src/test/java/practice/Test2.java
@@ -0,0 +1,27 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void test() {
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+	}
+
+}
---
+++ JenkinsEXE/src/test/java/practice/Test4.java
@@ -0,0 +1,26 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Test4 {
+	
+	@Test
+	public void test() {
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+		System.out.println("Hiii");
+	}
+}
